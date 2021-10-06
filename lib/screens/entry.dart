85cb0a0afc2be920eebcabf2ce4f40a5503d23de
nano-2-ly <dong991207@unist.ac.kr>
2021-10-07 07:28:45 +0900

    update google oauth

--- lib/screens/entry.dart
+++ lib/screens/entry.dart
@@ -20,7 +20,7 @@ class _EntryScreenState extends State<EntryScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      floatingActionButton: OAuthFAB(),
       backgroundColor: Colors.deepPurple,
       body: Center(
         child: Login(),
@@ -35,4 +35,59 @@ void hasAvailableSession() async{
   if(session.isSignedIn){
     Get.toNamed("/dashboard");
   }
+}
+
+Widget OAuthFAB(){
+  return Stack(
+    children: [
+      Positioned(
+        bottom: 10,
+        right: 0,
+        child: FloatingActionButton(
+          onPressed: () async{
+            try {
+              //print("Amplify.Auth.getCurrentUser()");
+              //print(await Amplify.Auth.getCurrentUser());
+              //Amplify.Auth.signOut();
+              SignInResult res = await Amplify.Auth.signInWithWebUI(provider: AuthProvider.google);
+              print("res.isSignedIn");
+              if(res.isSignedIn){
+                Get.toNamed("/dashboard");
+              }
+            } on AuthException catch (e) {
+              print("e.message");
+              print(e.message);
+            }
+
+          },
+          child: Center(child: Text("G", style: TextStyle(color: Colors.black),),),
+          backgroundColor: Colors.white,
+        ),
+      ),
+      Positioned(
+        bottom: 10,
+        right: 80,
+        child: FloatingActionButton(
+          onPressed: () async{
+            try {
+              //print("Amplify.Auth.getCurrentUser()");
+              //print(await Amplify.Auth.getCurrentUser());
+              //Amplify.Auth.signOut();
+              SignInResult res = await Amplify.Auth.signInWithWebUI(provider: AuthProvider.facebook);
+              print("res.isSignedIn");
+              if(res.isSignedIn){
+                Get.toNamed("/dashboard");
+              }
+            } on AuthException catch (e) {
+              print("e.message");
+              print(e.message);
+            }
+
+          },
+          child: Center(child: Text("f", style: TextStyle(color: Colors.blueAccent),),),
+          backgroundColor: Colors.white,
+        ),
+      ),
+    ],
+  );
 }
